d3502e4553e6a7579b0e2cd3b0703955b49c2556
andres <luis98_mc@hotmail.com>
2021-05-01 01:36:02 -0600

    Login parte 1
    primera parte de conexion a Mysql

---
+++ modelos/usuarios.modelos.php
@@ -0,0 +1,8 @@
+<?php
+    require_once "conexion.php";
+
+    class Modelousuarios{
+        static public function MdlMostrarUsuarios($tabla, $item, $valor){
+            $stmt = Conexion::conectar()->prpeare();
+        }
+    }
--- vistas/plantilla.php
+++ vistas/plantilla.php
@@ -38,46 +38,57 @@
  
 
 </head>
-<body class="hold-transition skin-red sidebar-collapse sidebar-mini">
+<body class="hold-transition skin-blue sidebar-collapse sidebar-mini login-page">
 <!-- Site wrapper -->
-<div class="wrapper">
+
   <!-- =============================================== -->
   <?php
 
-include "modulos/cabezote.php";
-include "modulos/menu.php";
+    if(isset($_SESSION["iniciarSesion"]) && $_SESSION["iniciarSesion"] == "ok"){
+      
+      echo '<div class="wrapper">';
 
-  if(isset($_GET["ruta"])){
-    if($_GET["ruta"] == "inicio" || 
-    $_GET["ruta"] == "usuarios" || 
-    $_GET["ruta"] == "categorias" || 
-    $_GET["ruta"] == "productos" || 
-    $_GET["ruta"] == "clientes" ||
-    $_GET["ruta"] == "ventas" || 
-    $_GET["ruta"] == "crear-ventas" ||  
-    $_GET["ruta"] == "reportes"
-    ){
-      include "modulos/".$_GET["ruta"].".php";
+        include "modulos/cabezote.php";
+        include "modulos/menu.php";
+
+        if(isset($_GET["ruta"])){
+          if($_GET["ruta"] == "inicio" || 
+          $_GET["ruta"] == "usuarios" || 
+          $_GET["ruta"] == "categorias" || 
+          $_GET["ruta"] == "productos" || 
+          $_GET["ruta"] == "clientes" ||
+          $_GET["ruta"] == "ventas" || 
+          $_GET["ruta"] == "crear-ventas" ||  
+          $_GET["ruta"] == "reportes"
+          ){
+            include "modulos/".$_GET["ruta"].".php";
+          }
+          else{
+            include "modulos/404.php";
+          }
+        }
+        else
+        {
+          include "modulos/inicio.php";
+        }
+          
+
+            include "modulos/footer.php";
+
+      echo '</div>';   
+
     }
     else{
-      include "modulos/404.php";
+      include "modulos/login.php";
     }
-  }
-  else
-  {
-    include "modulos/inicio.php";
-  }
-    
 
-    include "modulos/footer.php";
+  
 
   ?>
  
 
   <!-- =============================================== -->
 
-</div>
-<!-- ./wrapper -->
 
 <script src="vistas/js/plantilla.js"></script>
 </body>
